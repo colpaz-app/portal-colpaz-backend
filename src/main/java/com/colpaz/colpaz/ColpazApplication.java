99b54b30ac5070b918851a8ad00eca31cdce29a5
DiegoMendez01 <diegomendez01soft@gmail.com>
2025-06-24 10:36:29 -0500

    First commit

---
+++ src/main/java/com/colpaz/colpaz/ColpazApplication.java
@@ -0,0 +1,13 @@
+package com.colpaz.colpaz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColpazApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColpazApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/colpaz/colpaz/ColpazApplicationTests.java
@@ -0,0 +1,13 @@
+package com.colpaz.colpaz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColpazApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
